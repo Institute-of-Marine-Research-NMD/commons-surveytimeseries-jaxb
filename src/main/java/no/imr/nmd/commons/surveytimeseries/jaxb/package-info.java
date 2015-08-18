ee34913edc9473b6bae672acb41191437254ee35
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-08-18 10:11:03 +0200

    Added package info so that namespaces are correct.

---
+++ src/main/java/no/imr/nmd/commons/surveytimeseries/jaxb/package-info.java
@@ -0,0 +1,5 @@
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.imr.no/formats/surveytimeseries/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+    @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.imr.no/formats/surveytimeseries/v1", prefix = "")
+})
+package no.imr.nmd.commons.surveytimeseries.jaxb;
