a64533ff7ce684650180a8e3c3e3c14b0eda8d5b
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-23 10:43:21 +0200

    Added versioning and uses automatically generated package info

--- src/main/java/no/imr/nmd/commons/surveytimeseries/jaxb/package-info.java
+++
@@ -1,5 +0,0 @@
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.imr.no/formats/surveytimeseries/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
-    @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.imr.no/formats/surveytimeseries/v1", prefix = "")
-})
-package no.imr.nmd.commons.surveytimeseries.jaxb;
